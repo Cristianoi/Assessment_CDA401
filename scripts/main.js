8d328d4c53f1eb9dcdd5bd1968f58e433a578b51
cristianoi <cristianoi2406@gmail.com>
2017-03-13 23:02:03 +0000

    order form added

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ var current_image = 0;
 
 
 function main() {
-    
+    console.log("working");
     timeClock();
     
     imageCarousel();
@@ -21,26 +21,22 @@ function addZero(time) {
 function timeClock() {
     
     var today = new Date();
+    var date = new Date().toLocaleDateString();
     
-    var curr_day = addZero(today.getDay());
-    var curr_month = addZero(today.getMonth());
-    var curr_year = today.getFullYear();
-    
+       
     var curr_hour = addZero(today.getHours());
     var curr_minute = addZero(today.getMinutes());
     var curr_second = addZero(today.getSeconds());
     
     var curr_time = curr_hour + ":" + curr_minute + ":" + curr_second;
-    var curr_date = curr_day + "/" + curr_month + "/" + curr_year;
-    console.log(curr_date);
+    
+    
     document.getElementById("time").innerHTML = curr_time;
-    document.getElementById("date").innerHTML = 
-    curr_date;
+    document.getElementById("date").innerHTML = date;
     setTimeout("timeClock()", 500)
 }
 
 function imageCarousel() {
-    console.log("working")
     
     
     var image = document.getElementsByClassName("Pizzas");
@@ -66,4 +62,27 @@ function showMap() {
     var uluru = {lat: -25.363, lng: 131.044};
     var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
     var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
+function validateForm(name, val) {
+    console.log(name + " " + val );
+    switch (name) {
+        case "first_name": case "last_name":
+             document.getElementById(name).style.backgroundColor = "red";
+            break;
+        case "email":
+            break;
+        case "phone":
+            break;
+        
+    }
+}
+
+function reset_form() {
+    console.log("resetting..")
+    var form = document.getElementById("base_form");
+    console.log(form.base[1].value);
+    for (var i=0; i < document.form.base.length; i++) {
+        console.log("checked")
+    }
 }
