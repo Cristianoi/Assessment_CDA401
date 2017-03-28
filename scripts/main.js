9f5aff05549f3b1591b424d89d07de565278c055
cristianoi <cristianoi2406@gmail.com>
2017-03-28 23:40:18 +0100

    Improved contact form

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,38 @@
 var current_image = 0;
+var current_dadimage = 0;
+
+
 
 
 function main() {
     
     timeClock();
     
+    
+    
     imageCarousel();
     
-    showMap();
+    /*
+    if (window.location.pathname == '/order.html') {
+        myForm = document.getElementById("your_order");
+        myForm.addEventLinstener('change', updateOrder());
+    }
+    */
     
-    myForm = document.getElementById("your_order");
-    myForm.addEventLinstener("change", updateOrder());
+    if (location.pathname.split("/").pop() == 'next.html') {
+        countDown();
+        dadCarousel();
+    }
     
+    if (location.pathname.split("/").pop() == 'contact.html') {
+        
+        /*myForm.addEventListener("submit",validateForm);*/
+        document.getElementById("nameRequiredError").style.display = "none";
+        document.getElementById("surnameRequiredError").style.display = "none";
+        document.getElementById("emailRequiredError").style.display = "none";
+    }
 }
+
 
 function addZero(time) {
     
@@ -61,21 +81,76 @@ function imageCarousel() {
         
 }
 
+/*
+function validateForm() {
+    console.log("working");
+    var form = document.getElementById("contact_form");
+    
+    if (form.first_name.value == "") {
+        form.first_name.style.backgroundColor = "red";
+        document.getElementById("nameRequiredError").style.display = "inline";
+    } else {
+        form.first_name.style.backgroundColor = "white";
+        document.getElementById("nameRequiredError").style.display = "none";
+    }
+    if (form.last_name.value == "") {
+        form.last_name.style.backgroundColor = "red";
+        document.getElementById("surnameRequiredError").style.display = "inline";
+    } else {
+        form.last_name.style.backgroundColor = "white";
+        document.getElementById("surnameRequiredError").style.display = "none";
+        }
+    if (form.email.value == "") {
+        form.email.style.backgroundColor = "red";
+        document.getElementById("emailRequiredError").style.display = "inline";
+    } else {
+        form.email.style.backgroundColor = "white";
+        document.getElementById("emailRequiredError").style.display = "none";
+    }
+    
+    
+}
+*/
 
 
-function validateForm(name, val) {
-    console.log(name + " " + val );
-    switch (name) {
-        case "first_name": case "last_name":
-             document.getElementById(name).style.backgroundColor = "red";
-            break;
+function validateForm(inputName) {
+    console.log(inputName);
+    var form = document.getElementById("contact_form");
+    
+    switch (inputName) {
+        case "first_name":
+            if (form.first_name.value == "") {
+                form.first_name.style.backgroundColor = "red";
+                document.getElementById("nameRequiredError").style.display = "inline";
+            } else {
+                form.first_name.style.backgroundColor = "white";
+                document.getElementById("nameRequiredError").style.display = "none";
+            }
+        break;
+        case "last_name":
+            if (form.last_name.value == "") {
+            form.last_name.style.backgroundColor = "red";
+            document.getElementById("surnameRequiredError").style.display = "inline";
+        } else {
+            form.last_name.style.backgroundColor = "white";
+            document.getElementById("surnameRequiredError").style.display = "none";
+        }
+        break;
         case "email":
-            break;
-        case "phone":
-            break;
+            if (form.email.value == "") {
+            form.email.style.backgroundColor = "red";
+            document.getElementById("emailRequiredError").style.display = "inline";
+        } else {
+            
+            form.email.style.backgroundColor = "white";
+            document.getElementById("emailRequiredError").style.display = "none";
+        }
+        break;
+        
         
     }
 }
+
 
 function reset_form() {
     console.log("resetting..")
@@ -92,16 +167,53 @@ function reset_form() {
     for (var i = 0; i < form.extras.length; i++) {
         form.extras[i].checked = false
     }
+    document.getElementById("price").innerHTML = "0";
+    document.getElementById("choices").innerHTML = "";
+    document.getElementById("cost").innerHTML = "";
+    
 }
 
 function updateOrder(event) {
     var total = 0;
     var choices = "";
+    var cost = "";
     
-    console.log("form changed")
+    var form = document.getElementById("your_order");
+    
+    for (var i = 0; i < form.base.length; i++) {
+        if (form.base[i].checked) {
+            total += parseFloat(form.base[i].dataset.price);
+            choices += form.base.value + " Pizza ";
+            cost += "£" + form.base[i].dataset.price;
+        }
+    }
+    
+    for (var i = 0; i < form.toppings.length; i++) {
+        if (form.toppings[i].checked) {
+            total += parseFloat(form.toppings[i].dataset.price);
+            choices += "<br> &emsp; + " + form.toppings[i].value;
+            cost += "<br> £" + form.toppings[i].dataset.price;
+        }
+    }
+    
+    for (var i = 0; i < form.extras.length; i++) {
+        if (form.extras[i].checked) {
+            total += parseFloat(form.extras[i].dataset.price);
+            choices += "<br>" + form.extras[i].value;
+            cost += "<br> £" + form.extras[i].dataset.price;
+        }
+    }
+    
+    total= total.toFixed(2);
+    
+    document.getElementById("price").innerHTML = total;
+    document.getElementById("choices").innerHTML = choices;
+    document.getElementById("cost").innerHTML = cost;
 }
 
+
 function showMap() {
+    
     var myCenter = new google.maps.LatLng(50.9080, -1.4002);
     var mapCanvas = document.getElementById("myMap");
     var mapOptions = {center: myCenter, zoom: 14};
@@ -109,7 +221,38 @@ function showMap() {
     var marker = new google.maps.Marker({position:myCenter, animation: google.maps.Animation.BOUNCE});
   marker.setMap(map);
     var infowindow = new google.maps.InfoWindow({
-        content: "We are here!"
+        content: "We are here!",
+        
     });
     infowindow.open(map,marker);
+}
+
+
+function countDown() {
+    
+    
+    setTimeout("countDown()", 500);
+    /*
+    https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
+    */
+}
+
+function dadCarousel() {
+    
+    var image = document.getElementsByClassName("Dad");
+    
+    for (var i = 0; i < image.length; i++) {
+        image[i].style.display = "none";
+    }
+    for (var i = 0; i < image.length; i++) {
+        if (i == current_dadimage) {
+            image[i].style.display = "block";
+        }
+    }
+    current_dadimage++;
+    if (current_dadimage == image.length) {
+        current_dadimage = 0;
+    }
+    setTimeout("dadCarousel()",3000);
+        
 }
